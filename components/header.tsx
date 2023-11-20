53b1003e08f7aca27ea8a10f7e7e5bb6369a777a
Aniket Bamotra <aniketbamotra.11@gmail.com>
2023-11-20 21:35:58 +0530

    feat: added a base page for blogs on root, sorted routes for tech and community blogs, updated the import routes of all the assets with /blog prefix.

--- components/header.tsx
+++ components/header.tsx
@@ -17,9 +17,9 @@ export default function Header() {
     //   </Link>
     //   .
     // </h2>
-    <div className="header-container mb-20 mt-8 flex justify-between">
+    <div className="header-container mb-12 mt-8 flex justify-between">
       <Link href={"https://keploy.io/"} className="logo w-40 relative z-20">
-        <img src="/images/sidebyside-transparent.svg" alt="" />
+        <img src="/blog/images/sidebyside-transparent.svg" alt="" />
       </Link>
       <div className="menu md:block hidden">
         <ul className='flex gap-6 body text-lg'>
@@ -34,12 +34,12 @@ export default function Header() {
             <Link href={"/technology"}>Blog</Link>
           </li>
           <li className="bg-gradient-to-r from-orange-200 to-orange-100 bg-[length:0px_10px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500 hover:bg-[length:100%_10px] group-hover:bg-[length:100%_10px]">
-            <Link href={"/"}>Community</Link>
+            <Link href={"/community"}>Community</Link>
           </li>
         </ul>
       </div>
       
-      <div className="menu-icon md:hidden block"><img src="/images/Menu.svg" alt="menu" className='w-10' onClick={toggleMenu} /></div>
+      <div className="menu-icon md:hidden block"><img src="/blog/images/Menu.svg" alt="menu" className='w-10' onClick={toggleMenu} /></div>
       <div className={`${"menu-underlay w-screen h-screen absolute top-0 left-0 bg-black opacity-20"} ${!ToggleMenu ? "hidden" : "block"}`} onClick={()=>setToggleMenu(false)}></div>
       <div className={`${"mobile-menu flex pl-8 items-center absolute z-10 bg-white w-4/5 h-screen top-0 left-0 transition-all duration-300"} ${!ToggleMenu ? " -translate-x-full" : ""}`}>
       <ul className='body text-3xl font-bold flex flex-col gap-8 '>
@@ -54,7 +54,7 @@ export default function Header() {
             <Link href={"/technology"}>Blog</Link>
           </li>
           <li className="bg-gradient-to-r from-orange-200 to-orange-100 bg-[length:0px_10px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500 hover:bg-[length:100%_10px] group-hover:bg-[length:100%_10px]">
-            <Link href={"/"}>Community</Link>
+            <Link href={"/community"}>Community</Link>
           </li>
         </ul>
       </div>
