548c2568f6b07df4a996a184e8000535a2555593
Aniket Bamotra <aniketbamotra.11@gmail.com>
2023-11-28 11:28:56 +0530

    changes thumbnil image

--- components/post-body.tsx
+++ components/post-body.tsx
@@ -1,6 +1,9 @@
 import styles from './post-body.module.css'
 
 export default function PostBody({ content }) {
+
+
+
   return (
     <div className="2xl:max-w-4xl lg:max-w-3xl max-w-xl body mx-auto">
       <div
