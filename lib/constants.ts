1d153ed1977af7a4692da2481a6cf25cfe169306
Aniket Bamotra <aniketbamotra.11@gmail.com>
2023-11-28 12:37:30 +0530

    changes thumbnil image

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,4 +2,4 @@ import BlogThumbnil from '../public/images/thumbnil.png';
 export const EXAMPLE_PATH = 'cms-wordpress'
 export const CMS_NAME = 'WordPress'
 export const CMS_URL = 'https://wordpress.org'
-export const HOME_OG_IMAGE_URL = "https://keploy.io/wp/wp-content/uploads/2023/11/thumbnil-.png"
+export const HOME_OG_IMAGE_URL = "https://keploy.io/wp/wp-content/uploads/2023/11/thumbnil.png"
