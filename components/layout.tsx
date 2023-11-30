4e689d2f028d239022621f1dcb7d2dc3325966bf
Aniket Bamotra <aniketbamotra.11@gmail.com>
2023-11-30 20:05:52 +0530

    added google and clarity scripts

--- components/layout.tsx
+++ components/layout.tsx
@@ -11,6 +11,31 @@ export default function Layout({ preview, children }) {
         <main>{children}</main>
       </div>
       <Footer />
+
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-GYS09X6KHS"></script>
+      <script
+      type="text/javascript"
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-GYS09X6KHS');
+        `,
+      }}
+    />
+      <script
+      type="text/javascript"
+      dangerouslySetInnerHTML={{
+        __html: `
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window,document,"clarity","script","jymj0ktwcp");
+        `,
+      }}
+    />
     </>
   )
 }
