f680e741164943eca2680b549bc1a704b605d952
Aniket Bamotra <aniketbamotra.11@gmail.com>
2023-11-30 20:05:01 +0530

    added google and clarity scripts

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,8 +4,8 @@ import { EXAMPLE_PATH } from '../lib/constants'
 export default function Footer() {
   return (
     <footer className="bg-accent-1 border-t border-accent-2">
-      <Container>
-        {/* <div className="py-28 flex flex-col lg:flex-row items-center">
+      {/* <Container>
+        <div className="py-28 flex flex-col lg:flex-row items-center">
           <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
             Statically Generated with Next.js.
           </h3>
@@ -22,9 +22,9 @@ export default function Footer() {
             >
               View on GitHub
             </a>
-          </div> */}
-        {/* </div> */}
-      </Container>
+          </div>
+        </div>
+      </Container> */}
     </footer>
   )
 }
