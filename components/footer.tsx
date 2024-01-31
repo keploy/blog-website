1e44719ac53f2c2a0c97b277b4734397ea7ad924
Neha Gupta <gneha21@yahoo.in>
2024-01-31 13:46:06 +0530

    fix: footer logo and hydration error
    Signed-off-by: Neha Gupta <gneha21@yahoo.in>

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,4 +1,9 @@
 import Link from "next/link";
+import Logo from './logo';
+import Image from "next/image";
+import CNCF from "../public/images/cncf-landscape.png";
+import GSA from "../public/images/gsa.png";
+
 export default function Footer() {
   const sections = [
     {
@@ -22,37 +27,19 @@ export default function Footer() {
     },
   ];
   return (
-    <footer className="bg-[#00163d]">
+    <footer className="bg-secondary-300">
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="grid sm:grid-cols-12 gap-8 py-8 md:py-12">
+
           <div className="sm:col-span-12 lg:col-span-3">
             <div className="mb-2">
-              <a className="block" aria-label="Keploy" href="/">
-                <Link href={"https://keploy.io/"}>
-                  <img
-                    src="/blog/images/sidebyside-transparent.svg"
-                    width={130}
-                    alt=""
-                  />
-                </Link>
-              </a>
+              <Logo />
             </div>
             <div className="text-sm text-neutral-300">
-              <a
-                href="https://docs.keploy.io/security/"
-                className="text-[#e6e2d4] hover:text-[#ff914d] hover:underline transition duration-150 ease-in-out"
-              >
-                Security
-              </a>
-              {" · "}
-              <a
-                href="https://docs.keploy.io/privacy-policy/"
-                className="text-[#e6e2d4] hover:text-[#ff914d] hover:underline transition duration-150 ease-in-out"
-              >
-                Privacy Policy
-              </a>
+              <a href="https://keploy.io/docs/security/" className="text-neutral-300 hover:text-primary-300 hover:underline transition duration-150 ease-in-out">Security</a> · <a href="https://docs.keploy.io/privacy-policy/" className="text-neutral-300 hover:text-primary-300 hover:underline transition duration-150 ease-in-out">Privacy Policy</a>
             </div>
           </div>
+
           {sections.map((section, index) => (
             <div
               key={index}
@@ -77,18 +64,13 @@ export default function Footer() {
           ))}
 
           <div className="sm:col-span-6 md:col-span-4 lg:col-span-3">
-            <h6 className="text-[#ff914d] font-[500] mb-2 footer-font tracking-tighter mb-2">
-              Find us on CNCF Landscape
-            </h6>
-            <a href="https://landscape.cncf.io/card-mode?selected=keploy">
-              <img
-                width="200"
-                height="100"
-                src="https://keploy.io/images/cncf-landscape.png"
-                alt="CNCF Landscape"
-                className="w-full"
-              />
-            </a>
+            <h6 className="text-primary-300 font-medium mb-2">Find us on CNCF Landscape</h6>
+            <Link href="https://landscape.cncf.io/card-mode?selected=keploy">
+              <Image width={200} height={100} src={CNCF} alt="CNCF Landscape" className=" sm:w-9/12" />
+            </Link>
+            <Link href="https://blog.google/intl/en-in/introducing-the-eighth-cohort-of-google-for-startups-accelerator-india/">
+              <Image width={200} height={100} src={GSA} alt="CNCF Landscape" className=" sm:w-9/12 py-2" />
+            </Link>
           </div>
         </div>
 
@@ -110,7 +92,7 @@ export default function Footer() {
                     viewBox="0 0 32 32"
                     xmlns="http://www.w3.org/2000/svg"
                   >
-                    <path d="M24 11.5c-.6.3-1.2.4-1.9.5.7-.4 1.2-1 1.4-1.8-.6.4-1.3.6-2.1.8-.6-.6-1.5-1-2.4-1-1.7 0-3.2 1.5-3.2 3.3 0 .3 0 .5.1.7-2.7-.1-5.2-1.4-6.8-3.4-.3.5-.4 1-.4 1.7 0 1.1.6 2.1 1.5 2.7-.5 0-1-.2-1.5-.4 0 1.6 1.1 2.9 2.6 3.2-.3.1-.6.1-.9.1-.2 0-.4 0-.6-.1.4 1.3 1.6 2.3 3.1 2.3-1.1.9-2.5 1.4-4.1 1.4H8c1.5.9 3.2 1.5 5 1.5 6 0 9.3-5 9.3-9.3v-.4c.7-.5 1.3-1.1 1.7-1.8z"></path>
+                    <path d="M24 11.5c-.6.3-1.2.4-1.9.5.7-.4 1.2-1 1.4-1.8-.6.4-1.3.6-2.1.8-.6-.6-1.5-1-2.4-1-1.7 0-3.2 1.5-3.2 3.3 0 .3 0 .5.1.7-2.7-.1-5.2-1.4-6.8-3.4-.3.5-.4 1-.4 1.7 0 1.1.6 2.1 1.5 2.7-.5 0-1-.2-1.5-.4 0 1.6 1.1 2.9 2.6 3.2-.3.1-.6.1-.9.1-.2 0-.4 0-.6-.1.4 1.3 1.6 2.3 3.1 2.3-1.1.9-2.5 1.4-4.1 1.4H8c1.5.9 3.2 1.5 5 1.5 6 0 9.3-5 9.3-9.3v-.4c.7-.5 1.3-1.1 1.7-1.8z"/>
                   </svg>
                 </svg>
               </a>
@@ -126,7 +108,7 @@ export default function Footer() {
                   viewBox="0 0 32 32"
                   xmlns="http://www.w3.org/2000/svg"
                 >
-                  <path d="M16 8.2c-4.4 0-8 3.6-8 8 0 3.5 2.3 6.5 5.5 7.6.4.1.5-.2.5-.4V22c-2.2.5-2.7-1-2.7-1-.4-.9-.9-1.2-.9-1.2-.7-.5.1-.5.1-.5.8.1 1.2.8 1.2.8.7 1.3 1.9.9 2.3.7.1-.5.3-.9.5-1.1-1.8-.2-3.6-.9-3.6-4 0-.9.3-1.6.8-2.1-.1-.2-.4-1 .1-2.1 0 0 .7-.2 2.2.8.6-.2 1.3-.3 2-.3s1.4.1 2 .3c1.5-1 2.2-.8 2.2-.8.4 1.1.2 1.9.1 2.1.5.6.8 1.3.8 2.1 0 3.1-1.9 3.7-3.7 3.9.3.4.6.9.6 1.6v2.2c0 .2.1.5.6.4 3.2-1.1 5.5-4.1 5.5-7.6-.1-4.4-3.7-8-8.1-8z"></path>
+                  <path d="M16 8.2c-4.4 0-8 3.6-8 8 0 3.5 2.3 6.5 5.5 7.6.4.1.5-.2.5-.4V22c-2.2.5-2.7-1-2.7-1-.4-.9-.9-1.2-.9-1.2-.7-.5.1-.5.1-.5.8.1 1.2.8 1.2.8.7 1.3 1.9.9 2.3.7.1-.5.3-.9.5-1.1-1.8-.2-3.6-.9-3.6-4 0-.9.3-1.6.8-2.1-.1-.2-.4-1 .1-2.1 0 0 .7-.2 2.2.8.6-.2 1.3-.3 2-.3s1.4.1 2 .3c1.5-1 2.2-.8 2.2-.8.4 1.1.2 1.9.1 2.1.5.6.8 1.3.8 2.1 0 3.1-1.9 3.7-3.7 3.9.3.4.6.9.6 1.6v2.2c0 .2.1.5.6.4 3.2-1.1 5.5-4.1 5.5-7.6-.1-4.4-3.7-8-8.1-8z"/>
                 </svg>
               </a>
             </li>
