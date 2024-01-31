ba0f3d2b59459c20c07b8ec94aafbd9cb460b94b
Neha Gupta <gneha21@yahoo.in>
2024-01-31 13:53:37 +0530

    feat: blog website footer like main website (#8)
    * implement the footer section in the blog website
    
    Signed-off-by: hubsMIT1 <shivay4318@gmail.com>
    
    * replace Link tag with a
    
    Signed-off-by: hubsMIT1 <shivay4318@gmail.com>
    
    * fix: sitemap
    
    Signed-off-by: Neha Gupta <gneha21@yahoo.in>
    
    * Implement footer in the blog website #1236
    
    Signed-off-by: hubsMIT1 <shivay4318@gmail.com>
    
    * fix: footer logo and hydration error
    
    Signed-off-by: Neha Gupta <gneha21@yahoo.in>
    
    * fix: improve gitignore
    
    Signed-off-by: Neha Gupta <gneha21@yahoo.in>
    
    ---------
    
    Signed-off-by: hubsMIT1 <shivay4318@gmail.com>
    Signed-off-by: Neha Gupta <gneha21@yahoo.in>
    Co-authored-by: hubsMIT1 <shivay4318@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,40 @@ module.exports = {
         'accent-7': '#333',
         success: '#0070f3',
         cyan: '#79FFE1',
+        primary: {
+          50: "#FFF",
+          100: "#FFB575",
+          200: "#FFD0A0",
+          300: "#ff914d",
+          400: "#E67643",
+          500: "#C95919",
+        },
+        // Backgrounds, borders, subtle accents
+        secondary: {
+          50: "#FFFFFF",
+          100: "#29456E",
+          200: "#537FA1",
+          300: "#00163d",
+          400: "#000E27",
+          500: "#00061A",
+        },
+        // Backgrounds, borders, subtle accents
+        neutral: {
+          100: '#f5f5f5',
+          200: '#FFF',
+          300: '#e6e2d4',
+        },
+        accent: {
+          // Success messages, notifications, progress indicators
+          100: '#16704c',
+          // Highlights, icons, creative sections
+          200: '#5f3131',
+          // Attention-grabbing elements, notifications, important highlights
+          300: '#ef546b',
+          400: "#d9cd9c",
+          500: "#e6e2d4",
+          gradient: "linear-gradient(to right, #F5F5F5, #E35134, #FF914D, #BE2C1B, #6F0A0D)",
+        },
       },
       spacing: {
         28: '7rem',
