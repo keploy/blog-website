974ffa30a7797600e28e861c347c451eeff49ada
Aditya-eddy <aditya282003@gmail.com>
2024-02-08 15:48:06 +0530

    Added the codeHighlighting using Prismjs
    Signed-off-by: Aditya-eddy <aditya282003@gmail.com>

---
+++ components/prism-loader.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useEffect } from "react";
+import Prism from "prismjs";
+import "prismjs/themes/prism-okaidia.css";
+import "prismjs/components/prism-typescript";
+import "prismjs/components/prism-python"
+import "prismjs/components/prism-yaml"
+import "prismjs/components/prism-javascript"
+
+export default function PrismLoader() {
+  useEffect(() => {
+    Prism.highlightAll();
+  }, []);
+  return <div className="hidden"></div>;
+}
--- pages/community/[slug].tsx
+++ pages/community/[slug].tsx
@@ -1,3 +1,4 @@
+// slug.tsx
 import { useRouter } from "next/router";
 import ErrorPage from "next/error";
 import Head from "next/head";
@@ -13,11 +14,11 @@ import PostTitle from "../../components/post-title";
 import Tags from "../../components/tags";
 import { getAllPostsWithSlug, getPostAndMorePosts } from "../../lib/api";
 import { CMS_NAME } from "../../lib/constants";
+import PrismLoader from "../../components/prism-loader";
 
 export default function Post({ post, posts, preview }) {
   const router = useRouter();
   const morePosts = posts?.edges;
-  console.log();
 
   if (!router.isFallback && !post?.slug) {
     return <ErrorPage statusCode={404} />;
@@ -25,22 +26,19 @@ export default function Post({ post, posts, preview }) {
 
   return (
     <Layout preview={preview}>
+      <Head>
+        <title>{`${post.title} | Next.js Blog Example with ${CMS_NAME}`}</title>
+        <meta property="og:image" content={post.featuredImage?.node.sourceUrl} />
+      </Head>
+
       <Header />
       <Container>
         {router.isFallback ? (
           <PostTitle>Loading…</PostTitle>
         ) : (
           <>
+            <PrismLoader /> {/* Load Prism.js here */}
             <article>
-              <Head>
-                <title>
-                  {`${post.title} | Next.js Blog Example with ${CMS_NAME}`}
-                </title>
-                <meta
-                  property="og:image"
-                  content={post.featuredImage?.node.sourceUrl}
-                />
-              </Head>
               <PostHeader
                 title={post.title}
                 coverImage={post.featuredImage}
@@ -48,16 +46,12 @@ export default function Post({ post, posts, preview }) {
                 author={post.ppmaAuthorName}
                 categories={post.categories}
               />
-              <PostBody content={post.content} />
-              <footer>
-                {post.tags.edges.length > 0 && <Tags tags={post.tags} />}
-              </footer>
+              <PostBody content={post.content} /> {/* Use original content */}
+              <footer>{post.tags.edges.length > 0 && <Tags tags={post.tags} />}</footer>
             </article>
 
             <SectionSeparator />
-            {morePosts.length > 0 && (
-              <MoreStories posts={morePosts} isCommunity={true} />
-            )}
+            {morePosts.length > 0 && <MoreStories posts={morePosts} isCommunity={true} />}
           </>
         )}
       </Container>
@@ -65,11 +59,7 @@ export default function Post({ post, posts, preview }) {
   );
 }
 
-export const getStaticProps: GetStaticProps = async ({
-  params,
-  preview = false,
-  previewData,
-}) => {
+export const getStaticProps: GetStaticProps = async ({ params, preview = false, previewData }) => {
   const data = await getPostAndMorePosts(params?.slug, preview, previewData);
 
   return {
