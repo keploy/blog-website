1a139af309480159e11f1da84c5511b42e602492
Aditya Sharma <70089590+Aditya-eddy@users.noreply.github.com>
2024-02-09 13:02:07 +0530

    feat: changes in the meta tag and the layout tag and the preview site  (#9)
    * Changes in the meta tag and the layout tag and the changes that follows
    
    Signed-off-by: Aditya-eddy <aditya282003@gmail.com>
    
    * Removed meta tag from the slugs
    
    Signed-off-by: Aditya-eddy <aditya282003@gmail.com>
    
    * Added the X preview for the Website
    
    Signed-off-by: Aditya-eddy <aditya282003@gmail.com>
    
    ---------
    
    Signed-off-by: Aditya-eddy <aditya282003@gmail.com>

--- components/meta.tsx
+++ components/meta.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import { CMS_NAME, HOME_OG_IMAGE_URL } from "../lib/constants";
 
-export default function Meta() {
+export default function Meta({ featuredImage , Title , Description }) {
   return (
     <Head>
       <link
@@ -24,6 +24,13 @@ export default function Meta() {
       <link rel="manifest" href="blog/favicon/site.webmanifest" />
       <link rel="mask-icon" href="blog/favicon/Group.svg" color="#000000" />
       <link rel="shortcut icon" href="blog/favicon/Group" />
+
+      <meta name="twitter:card" content="summary_large_image"/>
+      <meta name="twitter:title" content={Title}/>
+      <meta name="twitter:description" content={Description}/>
+      {/* Twitter Summary card images must be at least 120x120px */}
+      <meta name="twitter:image" content={featuredImage}/>
+
       <meta name="msapplication-TileColor" content="#000000" />
       <meta
         name="msapplication-config"
@@ -31,13 +38,25 @@ export default function Meta() {
       />
       <meta name="theme-color" content="#000" />
       <link rel="alternate" type="application/rss+xml" href="blog/feed.xml" />
-      <meta name="description" content={`Kepoy Blog`} />
-      <meta property="og:title" content="Keploy's Blog" />
-      <meta property="og:description" content="A place for all the blogs." />
-      <meta property="og:image" content={HOME_OG_IMAGE_URL} />
-      <meta property="og:image:width" content='1200' />
-      <meta property="og:image:height" content='627' />
-
+      <meta name="description" content={Description} />
+      <meta property="og:title" content={Title} />
+      <meta property="og:description" content={Description} />
+      {featuredImage && (
+        <>
+          <meta property="og:image" content={featuredImage} />
+          <meta property="og:image:width" content="1200" />
+          <meta property="og:image:height" content="627" />
+          <meta name="twitter:image" content={featuredImage}/>
+        </>
+      )}
+      {!featuredImage && (
+        <>
+        <meta property="og:image" content={HOME_OG_IMAGE_URL}/>
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="627" />
+        <meta name="twitter:image" content={HOME_OG_IMAGE_URL}/>
+        </>
+      )}
       {/* <link
         rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/default.min.css"
