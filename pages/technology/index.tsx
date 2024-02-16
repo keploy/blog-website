517620a3fde6c579b5412b686cf7eff241a7f726
sahadat-sk <98450585+sahadat-sk@users.noreply.github.com>
2024-02-16 13:06:40 +0530

    fix: remove local getExcerpt function
    Signed-off-by: sahadat-sk <98450585+sahadat-sk@users.noreply.github.com>

--- pages/technology/index.tsx
+++ pages/technology/index.tsx
@@ -15,18 +15,6 @@ export default function Index({ allPosts: { edges }, preview }) {
   const excerpt = edges[0] ? getExcerpt(edges[0].node.excerpt, 50) : null;
   const morePosts = edges.slice(1);
 
-  function getExcerpt(content) {
-    const maxWords = 50;
-    // Split the content into an array of words
-    const words = content.split(" ");
-
-    // Ensure the excerpt does not exceed the maximum number of words
-    if (words.length > maxWords) {
-      return words.slice(0, maxWords).join(" ") + "...";
-    }
-
-    return content;
-  }
   return (
     <Layout
       preview={preview}
