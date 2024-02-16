75dbaca0de4d5bcad5883db146db620646698cd3
sahadat-sk <98450585+sahadat-sk@users.noreply.github.com>
2024-02-16 12:59:44 +0530

    fix: restore cta button and open source logo
    Signed-off-by: sahadat-sk <98450585+sahadat-sk@users.noreply.github.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,16 +24,16 @@ export default function Index({ communityPosts, technologyPosts, preview }) {
       <Header />
       <Container>
         <div className="">
-          <div className="home-container md:mb-0 mb-4 flex lg:flex-nowrap flex-col lg:flex-row justify-between items-center">
+          <div className="home-container md:mb-0 mb-4 flex lg:flex-nowrap flex-wrap-reverse justify-evenly items-center">
             <div className="content">
-              <h2 className=" font-bold 2xl:text-7xl text-6xl lg:text-8xl text-orange-400 ">
+              <h2 className="heading1 font-bold 2xl:text-7xl text-6xl text-orange-400">
                 Keploy's Blog
               </h2>
-              <p className="content-body body 2xl:text-xl text-sm  mt-2 text-center">
+              <p className="content-body body 2xl:text-2xl text-lg w-max mt-6">
                 Elevate Your Tech Insight. Navigating the <br />
                 Frontiers of Innovation and Integration.
               </p>
-              {/* <div className="btn-wrapper flex gap-4 mt-6">
+              <div className="btn-wrapper flex gap-4 mt-6">
                 <Link
                   href="/technology"
                   className="tech-blog cursor-pointer 2xl:text-xl text-lg body md:px-12 px-8 py-2 border-2 border-black rounded-xl hover:bg-orange-400 hover:text-white hover:border-orange-400 "
@@ -46,29 +46,31 @@ export default function Index({ communityPosts, technologyPosts, preview }) {
                 >
                   Community
                 </Link>
-              </div> */}
+              </div>
             </div>
+
             <div className="blog-hero-img">
               <img
                 src="/blog/images/blog-bunny.png"
                 alt="hero image"
                 width={600}
                 height={600}
+                className=""
               />
             </div>
           </div>
-          <TopBlogs
-            communityPosts={communityPosts}
-            technologyPosts={technologyPosts}
-          />
-          {/* <div className="open-source-vector-container bottom-9 mb-12 flex md:justify-start justify-center">
+          <div className="open-source-vector-container bottom-9 mb-12 flex md:justify-start justify-center">
             <img
               src="/blog/images/open-source-vector.png"
               alt="vector"
               className=" spin-anim"
             />
-          </div> */}
+          </div>
         </div>
+        <TopBlogs
+          communityPosts={communityPosts}
+          technologyPosts={technologyPosts}
+        />
       </Container>
     </Layout>
   );
