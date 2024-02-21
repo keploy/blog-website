0cc3870becbe4a3a08be71487bf847d857c44bb3
Swapnoneel Saha <swapnoneelsaha111@gmail.com>
2024-02-21 14:52:27 +0530

    Added the original tweets
    Signed-off-by: Swapnoneel Saha <swapnoneelsaha111@gmail.com>

--- components/tweets.tsx
+++ components/tweets.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
 
-const Tweets = ({avatar, id, content}) => {
+const Tweets = ({avatar, name, id, post, content}) => {
   return (
     <>
-        <div className="bg-gray-100 border p-6 rounded-md lg:hover:shadow-md transition m-1">
+        <a className="bg-gray-100 border p-6 rounded-md lg:hover:shadow-md transition m-1" href={post}>
             <div className="flex flex-row gap-2 items-center">
                 <img src={avatar} alt="profile_image" className="rounded-full h-12"/>
-                <div className="font-bold">@{id}</div>
+                <div className="flex flex-col justify-center">
+                  <div className="font-bold">{name}</div>
+                  <div className="">@{id}</div>
+                </div>
             </div>
             <div className="pt-2">{content}</div>
             <div className=""></div>
-        </div>
+        </a>
     </>
   )
 };
