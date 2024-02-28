297f52560a5e973fe8eb24e7429fc57c1e00c844
Aditya Sharma <70089590+Aditya-eddy@users.noreply.github.com>
2024-02-28 13:02:38 +0530

    feat: beautify tags and add tag based posts filtering (#14)
    * Adding the tags
    
    Signed-off-by: Aditya-eddy <aditya282003@gmail.com>
    
    * Adding the required changes
    
    Signed-off-by: Aditya-eddy <aditya282003@gmail.com>
    
    * adding the slug posts
    
    Signed-off-by: Aditya-eddy <aditya282003@gmail.com>
    
    * changing the title in preview
    
    Signed-off-by: Aditya-eddy <aditya282003@gmail.com>
    
    * Also fixing the issue breaking favicons
    
    Signed-off-by: Aditya-eddy <aditya282003@gmail.com>
    
    ---------
    
    Signed-off-by: Aditya-eddy <aditya282003@gmail.com>
    Signed-off-by: Md Athar <mdathar4403@gmail.com>

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -5,7 +5,7 @@ import MoreStories from "../../components/more-stories";
 import HeroPost from "../../components/hero-post";
 import Intro from "../../components/intro";
 import Layout from "../../components/layout";
-import { getAllPostsForHome } from "../../lib/api";
+import { getAllPostsForHome, getAllTags } from "../../lib/api";
 import { CMS_NAME } from "../../lib/constants";
 import Header from "../../components/header";
 
@@ -55,7 +55,6 @@ export default function Community({ allPosts: { edges }, preview }) {
 
 export const getStaticProps: GetStaticProps = async ({ preview = false }) => {
   const allPosts = await getAllPostsForHome(preview);
-
   return {
     props: { allPosts, preview },
     revalidate: 10,
