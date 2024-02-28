556e866ebfdcd0f9d42f93858fb7dfc074c1ba7e
Aditya Sharma <70089590+Aditya-eddy@users.noreply.github.com>
2024-02-28 13:02:38 +0530

    feat: beautify tags and add tag based posts filtering (#14)
    * Adding the tags
    
    Signed-off-by: Aditya-eddy <aditya282003@gmail.com>
    
    * Adding the required changes
    
    Signed-off-by: Aditya-eddy <aditya282003@gmail.com>
    
    * adding the slug posts
    
    Signed-off-by: Aditya-eddy <aditya282003@gmail.com>
    
    * changing the title in preview
    
    Signed-off-by: Aditya-eddy <aditya282003@gmail.com>
    
    * Also fixing the issue breaking favicons
    
    Signed-off-by: Aditya-eddy <aditya282003@gmail.com>
    
    ---------
    
    Signed-off-by: Aditya-eddy <aditya282003@gmail.com>

---
+++ components/TagsPostPreview.tsx
@@ -0,0 +1,49 @@
+import Avatar from "./avatar";
+import Date from "./date";
+import CoverImage from "./cover-image";
+import Link from "next/link";
+
+export default function TagsPostPreview({
+  title,
+  coverImage,
+  date,
+  excerpt,
+  author,
+  slug,
+  isCommunity
+}) {
+    const basePath = isCommunity && isCommunity.edges && isCommunity.edges[0] && isCommunity.edges[0].node.name === "community" ? "/community" : "/technology";
+
+  return (
+    <div className="bg-gray-100 border p-6 rounded-md   lg:hover:shadow-md transition">
+      <div className="mb-5">
+        {coverImage && (
+          <CoverImage
+            title={title}
+            coverImage={coverImage}
+            slug={slug}
+            isCommunity={isCommunity}
+          />
+        )}
+      </div>
+      <h3 className="text-2xl leading-snug leading-none heading1 font-bold">
+        <Link
+          href={`${basePath}/${slug}`}
+          className="bg-gradient-to-r from-orange-200 to-orange-100 bg-[length:0px_10px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500 hover:bg-[length:100%_10px] group-hover:bg-[length:100%_10px]"
+          dangerouslySetInnerHTML={{ __html: title }}
+        ></Link>
+      </h3>
+      <div className="flex items-center gap-4">
+        <Avatar author={author ? author : "Anonymous"} />
+        <div className="divider bg-orange-700 h-1 w-1 rounded-full"></div>
+        <div className="text-md mb-4 pt-4">
+          <Date dateString={date} />
+        </div>
+      </div>
+      <div
+        className="text-sm leading-normal mb-4 body text-slate-600"
+        dangerouslySetInnerHTML={{ __html: excerpt }}
+      />
+    </div>
+  );
+}
