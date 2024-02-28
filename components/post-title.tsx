179471a12fe238ac85256e68d6d8689d8392b845
Kunal Agrawal <kunal.py3@gmail.com>
2024-02-28 17:24:07 +0530

    Fix: Title Inconsistencies in the blog page

--- components/post-title.tsx
+++ components/post-title.tsx
@@ -1,8 +1,13 @@
-export default function PostTitle({ children }) {
+export default function PostTitle({ children }: { children: string }) {
+  function toTitleCase(str) {
+    return str
+      .toLowerCase()
+      .replace(/\b\w/g, (word) => word.toUpperCase());
+  }
+  const finalString = toTitleCase(children);
   return (
-    <h1
-      className="text-4xl md:text-4xl lg:text-5xl max-w-4xl mx-auto heading1 font-bold tracking-normal leading-normal md:leading-none mb-4 text-center"
-      dangerouslySetInnerHTML={{ __html: children }}
-    />
-  )
+    <h1 className="text-4xl md:text-4xl lg:text-5xl max-w-4xl mx-auto heading1 font-bold tracking-normal leading-normal md:leading-none mb-4 text-center">
+      {finalString}
+    </h1>
+  );
 }
