f1619f64b4294ec97ac35a3ede4184742bcd78fb
Swapnoneel Saha <swapnoneelsaha111@gmail.com>
2024-02-29 19:49:22 +0530

    Fixed Links and Tablet Responsiveness
    Signed-off-by: Swapnoneel Saha <swapnoneelsaha111@gmail.com>

--- components/tweets.tsx
+++ components/tweets.tsx
@@ -1,30 +1,38 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
 
-const Tweets = ({avatar, name, id, post, content}) => {
+const Tweets = ({ avatar, name, id, post, content }) => {
   return (
     <>
-        <a className="bg-gray-100 border p-6 rounded-md lg:hover:shadow-md transition m-1" href={post}>
-            <div className="flex flex-row items-center justify-between">
-              <div className="flex flex-row gap-2 items-center">
-                <img src={avatar} alt="profile_image" className="rounded-full h-12"/>
-                <div className="flex flex-col justify-center">
-                  <div className="font-bold">{name}</div>
-                  <div className="">@{id}</div>
-                </div>
-              </div>
-              <Image 
-                src="blog/favicon/x-twitter.svg"
-                width={20}
-                height={20}
-                alt="twitter icon"
-              />
+      <a
+        className="bg-gray-100 border p-6 rounded-md lg:hover:shadow-md transition m-1"
+        href={post}
+        target="_blank"
+      >
+        <div className="flex flex-row items-center justify-between">
+          <div className="flex flex-row gap-2 items-center">
+            <img
+              src={avatar}
+              alt="profile_image"
+              className="rounded-full h-12"
+            />
+            <div className="flex flex-col justify-center">
+              <div className="font-bold">{name}</div>
+              <div className="">@{id}</div>
             </div>
-            <div className="pt-2">{content}</div>
-            <div className=""></div>
-        </a>
+          </div>
+          <Image
+            src="blog/favicon/x-twitter.svg"
+            width={20}
+            height={20}
+            alt="twitter icon"
+          />
+        </div>
+        <div className="pt-2">{content}</div>
+        <div className=""></div>
+      </a>
     </>
-  )
+  );
 };
 
 export default Tweets;
