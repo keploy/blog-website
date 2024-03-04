92f3d893cad82bc9efc889fb6a8ddde596bd8a31
Kunal Agrawal <kunal.py3@gmail.com>
2024-03-04 20:14:56 +0530

    Change: Categories component, subcomp for post-header

--- components/categories.tsx
+++ components/categories.tsx
@@ -1,16 +1,18 @@
-export default function Categories({ categories }) {
+export default function Categories({ categories, className =""}) {
   return (
-    <span className="ml-1">
-      under
+    <div className={"flex items-center gap-x-2" + " " + className}>
       {categories.edges.length > 0 ? (
         categories.edges.map((category, index) => (
-          <span key={index} className="ml-1">
+          <span
+            key={index}
+            className="ml-1 font-bold rounded-full uppercase bg-gradient-to-b from-orange-600 to-orange-300 text-clip text-transparent bg-clip-text text-base"
+          >
             {category.node.name}
           </span>
         ))
       ) : (
         <span className="ml-1">{categories.edges.node.name}</span>
       )}
-    </span>
-  )
+    </div>
+  );
 }
