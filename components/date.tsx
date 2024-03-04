0ff2274f3b2658f7c2b2c679bebe43deeeeaaa79
Kunal Agrawal <kunal.py3@gmail.com>
2024-03-04 20:15:54 +0530

    Add: Extra parameter className for date component

--- components/date.tsx
+++ components/date.tsx
@@ -1,6 +1,10 @@
-import { parseISO, format } from 'date-fns'
+import { parseISO, format } from "date-fns";
 
-export default function Date({ dateString }) {
-  const date = parseISO(dateString)
-  return <time dateTime={dateString}>{format(date, 'LLLL	d, yyyy')}</time>
+export default function Date({ dateString, className = "" }) {
+  const date = parseISO(dateString);
+  return (
+    <div className={className}>
+      <time dateTime={dateString}>{format(date, "LLLL	d, yyyy")}</time>
+    </div>
+  );
 }
