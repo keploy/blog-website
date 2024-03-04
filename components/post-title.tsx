f7190e4e42c8f3856cee549539482fa57b8420db
Kunal Agrawal <kunal.py3@gmail.com>
2024-03-04 20:17:57 +0530

    Fix: Post Title Component Layout

--- components/post-title.tsx
+++ components/post-title.tsx
@@ -1,7 +1,7 @@
 export default function PostTitle({ children }) {
   return (
     <h1
-      className="text-4xl md:text-4xl lg:text-5xl max-w-4xl mx-auto heading1 font-bold tracking-normal leading-normal md:leading-none mb-4 text-center"
+      className="text-4xl md:text-4xl lg:text-5xl max-w-4xl mx-auto heading1 font-bold tracking-normal leading-normal md:leading-none mb-4"
       dangerouslySetInnerHTML={{ __html: children }}
     />
   )
