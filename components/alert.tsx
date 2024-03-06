0b61348af4c9d81771b88255280f4afb9e834529
Neha Gupta <gneha21@yahoo.in>
2024-03-06 20:14:55 +0530

    fix: build errors
    Signed-off-by: Neha Gupta <gneha21@yahoo.in>

--- components/alert.tsx
+++ components/alert.tsx
@@ -1,6 +1,7 @@
 import Container from './container'
 import cn from 'classnames'
 import { EXAMPLE_PATH } from '../lib/constants'
+import Link from "next/link";
 
 export default function Alert({ preview }) {
   return (
@@ -15,12 +16,12 @@ export default function Alert({ preview }) {
           {preview ? (
             <>
               This is a page preview.{' '}
-              <a
+              <Link
                 href="/api/exit-preview"
                 className="underline hover:text-cyan duration-200 transition-colors"
               >
                 Click here
-              </a>{' '}
+              </Link>{' '}
               to exit preview mode.
             </>
           ) : (
