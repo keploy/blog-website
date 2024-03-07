f2d0703f29b320a7883dd2130994b7be034e237b
Kunal Agrawal <92196937+its-kunal@users.noreply.github.com>
2024-03-07 13:24:52 +0530

    Fix: keploy Logo, Title Inconsistencies, View All Posts (#21)
    * Fix: Missing Alt Attribute in Header for /blog
    
    * Fix: Title Inconsistencies in the blog page
    
    * Fix:View All Posts Link looks more subtle
    
    * fix: view all post body re renders when new content is loaded
    
    * Add: content as a dependency for useEffect hook
    
    ---------
    
    Co-authored-by: Neha Gupta <gneha21@yahoo.in>

--- pages/technology/[slug].tsx
+++ pages/technology/[slug].tsx
@@ -36,7 +36,7 @@ export default function Post({ post, posts, preview }) {
   if (!post || !post.content) {
     return ''; // or handle this case differently based on your requirements
   }
-  // console.log(post.content);
+// console.log(post.content);
   return (
     <Layout
       preview={preview}
@@ -79,7 +79,7 @@ export default function Post({ post, posts, preview }) {
           {morePosts.length > 0 && (
             <MoreStories posts={morePosts} isCommunity={false} />
           )}
-        </article>  
+        </article>
       </Container>
     </Layout>
   );
