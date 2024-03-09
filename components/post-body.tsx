2d69b42b0bdf83e4bc25112fe4e464a511e8775e
Kunal Agrawal <kunal.py3@gmail.com>
2024-03-09 16:41:40 +0530

    Add Subscription component in single post page

--- components/post-body.tsx
+++ components/post-body.tsx
@@ -138,7 +138,6 @@ export default function PostBody({ content, authorName }) {
   };
 
 
-
   return (
     <div className="flex flex-col lg:flex-row">
       {/* Table of Contents */}
--- components/subscribe-newsletter.tsx
+++ components/subscribe-newsletter.tsx
@@ -145,4 +145,3 @@ export default function SubscribeNewsletter() {
     </div>
   );
 }
-
