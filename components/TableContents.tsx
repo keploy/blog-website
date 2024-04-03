7974efffe9bb63d51fa8c58324158d76cbeac17e
sahadat-sk <mss.21U10442@btech.nitdgp.ac.in>
2024-04-03 20:50:05 +0530

    fix: overflow in table of content
    Signed-off-by: sahadat-sk <mss.21U10442@btech.nitdgp.ac.in>

--- components/TableContents.tsx
+++ components/TableContents.tsx
@@ -1,5 +1,8 @@
+import React, { useState, useEffect } from "react";
+
 function TOCItem({ id, title, type, onClick }) {
-  const itemClasses = "mb-1 text-slate-600 space-y-1";
+  const itemClasses =
+    "mb-1 text-slate-600 space-y-1";
 
   // Calculate margin left based on heading type
   let marginLeft;
@@ -24,7 +27,7 @@ function TOCItem({ id, title, type, onClick }) {
     <li className={itemClasses} style={{ marginLeft }}>
       <button
         onClick={() => onClick(id)}
-        className="block w-full py-1 text-sm text-left text-black transition-all duration-150 ease-in-out rounded-md opacity-75 hover:text-orange-500 hover:opacity-100"
+        className="block py-1 w-full rounded-md text-left text-sm text-black hover:text-orange-500 opacity-75 hover:opacity-100 transition-all duration-150 ease-in-out"
       >
         {title}
       </button>
@@ -48,12 +51,29 @@ export default function TOC({ headings }) {
     }
   };
 
+  // State to track screen width
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  // Function to check if screen width is small
+  const checkScreenSize = () => {
+    setIsSmallScreen(window.innerWidth <= 1100); // Adjust breakpoint as needed
+  };
+
+  useEffect(() => {
+    checkScreenSize(); // Initial check
+    window.addEventListener("resize", checkScreenSize); // Event listener for screen resize
+    return () => {
+      window.removeEventListener("resize", checkScreenSize); // Cleanup on component unmount
+    };
+  }, []);
+
+  // Render dropdown if on a small screen, otherwise render regular TOC
   return (
     <>
-      <div className="left-0 inline-block p-4 lg:hidden top-20">
-        <div className="mb-2 text-lg font-semibold">Table of Contents</div>
+      <div className="inline-block lg:hidden left-0 top-20 p-4">
+        <div className="text-lg font-semibold mb-2">Table of Contents</div>
         <select
-          className="block w-full px-4 py-2 text-sm leading-tight bg-white border border-gray-300 rounded-md shadow-sm hover:border-gray-400 focus:outline-none focus:shadow-outline"
+          className="block w-full bg-white border border-gray-300 hover:border-gray-400 px-4 py-2 rounded-md shadow-sm text-sm leading-tight focus:outline-none focus:shadow-outline"
           onChange={(e) => handleItemClick(e.target.value)}
         >
           {headings.map((item, index) => (
@@ -63,8 +83,8 @@ export default function TOC({ headings }) {
           ))}
         </select>
       </div>
-      <div className="sticky left-0 hidden h-auto p-4 overflow-y-auto lg:inline-block top-20 bg-inherit">
-        <div className="mb-2 text-lg font-semibold">Table of Contents</div>
+      <div className="hidden lg:inline-block left-0 top-20 overflow-y-auto h-auto bg-inherit p-4 sticky max-h-[90vh]">
+        <div className="text-lg font-semibold mb-2">Table of Contents</div>
         <nav>
           <ul className="pl-0 leading-5">
             {headings.map((item, index) => (
