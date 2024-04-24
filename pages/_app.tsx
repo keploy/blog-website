c8b0ceafa7ab13f521dee5516d9a53f0474812f3
Md Sahadat Sk <98450585+sahadat-sk@users.noreply.github.com>
2024-04-24 23:13:34 +0530

    fix: resolve merge conflicts (#34)
    Signed-off-by: sahadat-sk <mss.21U10442@btech.nitdgp.ac.in>
    Co-authored-by: Neha Gupta <gneha21@yahoo.in>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,42 @@
-import { AppProps } from 'next/app'
-import '../styles/index.css'
+import { AppProps } from 'next/app';
+import '../styles/index.css';
+import Router from "next/router";
+
+import { AnimatePresence } from "framer-motion";
+import { useEffect, useState } from "react";
+import dynamic from 'next/dynamic'
+ 
+const PageLoader = dynamic(() => import('../components/PageLoader'), {
+  ssr: false,
+})
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    // Used for page transition
+    const startLoader = () => {
+      setLoading(true);
+    };
+    const stopLoader = () => {
+      setLoading(false);
+    };
+    Router.events.on("routeChangeStart", startLoader);
+    Router.events.on("routeChangeComplete", stopLoader);
+    Router.events.on("routeChangeError", stopLoader);
+    return () => {
+      Router.events.off("routeChangeStart", startLoader);
+      Router.events.off("routeChangeComplete", stopLoader);
+      Router.events.off("routeChangeError", stopLoader);
+    };
+  }, []);
+
+  return (
+    <>
+      <AnimatePresence>
+        {loading ? <PageLoader /> : <Component {...pageProps} />}
+      </AnimatePresence>
+    </>
+  );
 }
 
 export default MyApp
