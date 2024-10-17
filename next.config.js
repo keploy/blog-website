df336fdb283ac22fa6957e26f62760dface3cd70
Yash Khare <khareyash05@gmail.com>
2024-10-17 11:38:36 +0530

    fix url config
    Signed-off-by: Yash Khare <khareyash05@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,15 +1,13 @@
-const apiUrl = process.env.WORDPRESS_API_URL || "https://wp.keploy.io/graphql";
-
-try {
-  new URL(apiUrl);
-} catch (err) {
+if (!URL.canParse(process.env.WORDPRESS_API_URL)) {
   throw new Error(`
     Please provide a valid WordPress instance URL.
     Add to your environment variables WORDPRESS_API_URL.
-  `);
+  `)
 }
 
-const { protocol, hostname, port, pathname } = apiUrl
+const { protocol, hostname, port, pathname } = new URL(
+  process.env.WORDPRESS_API_URL
+)
 
 /** @type {import('next').NextConfig} */
 module.exports = {
