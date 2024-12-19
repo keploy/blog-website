6fd977253b90ad254a1e669db1bd82bb3f5311ae
ab7022 <bayees1@gmail.com>
2024-12-19 18:00:28 +0530

    removed cn function from code
    Signed-off-by: ab7022 <bayees1@gmail.com>

--- components/Marquee.tsx
+++ components/Marquee.tsx
@@ -1,4 +1,3 @@
-import { cn } from "../lib/utils";
 interface MarqueeProps {
   className?: string;
   reverse?: boolean;
@@ -21,26 +20,14 @@ export function Marquee({
   return (
     <div
       {...props}
-      className={cn(
-        "group flex overflow-hidden p-2 [--duration:40s] [--gap:1rem] [gap:var(--gap)]",
-        {
-          "flex-row": !vertical,
-          "flex-col": vertical,
-        },
-        className,
-      )}
+      className={`group flex overflow-hidden p-2 [--duration:40s] [--gap:1rem] [gap:var(--gap)] ${vertical ? 'flex-col' : 'flex-row'} ${className}`}
     >
       {Array(repeat)
         .fill(0)
         .map((_, i) => (
           <div
             key={i}
-            className={cn("flex shrink-0 justify-around [gap:var(--gap)]", {
-              "animate-marquee flex-row": !vertical,
-              "animate-marquee-vertical flex-col": vertical,
-              "group-hover:[animation-play-state:paused]": pauseOnHover,
-              "[animation-direction:reverse]": reverse,
-            })}
+            className={`flex shrink-0 justify-around [gap:var(--gap)] ${vertical ? 'animate-marquee-vertical flex-col' : 'animate-marquee flex-row'} ${pauseOnHover ? 'group-hover:[animation-play-state:paused]' : ''} ${reverse ? '[animation-direction:reverse]' : ''}`}
           >
             {children}
           </div>
