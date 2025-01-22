73d28c6de2e7258dfd84425c56c90501822c8cc5
Yash Khare <yash2010118@akgec.ac.in>
2025-01-22 14:28:53 +0530

    refactor: blog website  (#78)
    * chore: blog website fix
    
    Signed-off-by: Yash Khare <khareyash05@gmail.com>
    
    * remove redundant api
    
    Signed-off-by: Yash Khare <khareyash05@gmail.com>
    
    * add new twitter logo
    
    Signed-off-by: Yash Khare <khareyash05@gmail.com>
    
    * copy url functionality
    
    Signed-off-by: Yash Khare <khareyash05@gmail.com>
    
    * feat: copy url functionality
    
    Signed-off-by: Yash Khare <khareyash05@gmail.com>
    
    * feat: latest blog post on home page
    
    Signed-off-by: Yash Khare <khareyash05@gmail.com>
    
    * read more section
    
    Signed-off-by: Yash Khare <khareyash05@gmail.com>
    
    * refactor top tech blogs and community page
    
    Signed-off-by: Yash Khare <khareyash05@gmail.com>
    
    * simplify api for technology and community
    
    Signed-off-by: Yash Khare <khareyash05@gmail.com>
    
    * fix: toc in all blogs
    
    Signed-off-by: Animesh Pathak <kurosakiichigo.songoku@gmail.com>
    
    ---------
    
    Signed-off-by: Yash Khare <khareyash05@gmail.com>
    Signed-off-by: Animesh Pathak <kurosakiichigo.songoku@gmail.com>
    Signed-off-by: Animesh Pathak <53110238+Sonichigo@users.noreply.github.com>
    Co-authored-by: Animesh Pathak <kurosakiichigo.songoku@gmail.com>
    Co-authored-by: Animesh Pathak <53110238+Sonichigo@users.noreply.github.com>

--- components/topBlogs.tsx
+++ components/topBlogs.tsx
@@ -4,61 +4,92 @@ import Link from "next/link";
 
 const TopBlogs = ({ communityPosts, technologyPosts }) => {
   return (
-    <>
-      <h3 className="text-center lg:text-left bg-gradient-to-r from-orange-200 to-orange-100 bg-[length:100%_20px] bg-no-repeat bg-left-bottom w-max mb-6 text-3xl lg:text-4xl heading1 md:text-4xl font-bold tracking-tighter leading-tight mt-16">
-        Top Technology Blogs
-      </h3>
+    <section className="py-12 px-4 md:px-8 lg:px-16 ">
+      <div className="mb-16">
+        <h3 className="text-center lg:text-left bg-gradient-to-r from-orange-200 to-orange-100 bg-[length:100%_20px] bg-no-repeat bg-left-bottom w-max mb-6 text-3xl lg:text-4xl heading1 md:text-4xl font-bold tracking-tighter leading-tight mt-16">
+          Top Technology Blogs
+          <span className="absolute left-0 bottom-0 w-16 h-1 bg-gradient-to-r from-orange-400 to-orange-600"></span>
+        </h3>
 
-      <div className="grid grid-cols-1 xl:grid-cols-3 lg:grid-cols-2 md:gap-x-8 lg:gap-x-8 gap-y-16 md:gap-y-16 mb-4">
-        {technologyPosts.map(({ node }) => (
-          <PostPreview
-            key={node.slug}
-            title={node.title}
-            coverImage={node.featuredImage}
-            date={node.date}
-            author={node.ppmaAuthorName}
-            slug={node.slug}
-            excerpt={getExcerpt(node.excerpt, 20)}
-            isCommunity={false}
-          />
-        ))}
-      </div>
-      <div className="w-full flex justify-end bold underline text-md">
-        <Link
-          href="/technology"
-          className="text-secondary-100 hover:text-secondary-200 hover:translate-x-0.5 transition "
-        >
-          See all technology blogs
-        </Link>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+          {technologyPosts.map(({ node }) => (
+            <PostPreview
+              key={node.slug}
+              title={node.title}
+              coverImage={node.featuredImage}
+              date={node.date}
+              author={node.ppmaAuthorName}
+              slug={node.slug}
+              excerpt={getExcerpt(node.excerpt, 20)}
+              isCommunity={false}
+            />
+          ))}
+        </div>
+
+        <div className="mt-6 flex justify-end">
+          <Link
+            href="/technology"
+            className="text-orange-500 hover:text-orange-600 font-semibold flex items-center transition-colors duration-300"
+          >
+            See all technology blogs
+            <span className="ml-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
+              </svg>
+            </span>
+          </Link>
+        </div>
       </div>
 
-      <h3 className="bg-gradient-to-r from-orange-200 to-orange-100 bg-[length:100%_20px] bg-no-repeat bg-left-bottom w-max mb-6 text-3xl lg:text-4xl heading1 md:text-4xl font-bold tracking-tighter leading-tight mt-16">
-        Top Community Blogs
-      </h3>
+      <div>
+        <h3 className="text-center lg:text-left bg-gradient-to-r from-orange-200 to-orange-100 bg-[length:100%_20px] bg-no-repeat bg-left-bottom w-max mb-6 text-3xl lg:text-4xl heading1 md:text-4xl font-bold tracking-tighter leading-tight mt-16">
+          Top Community Blogs
+          <span className="absolute left-0 bottom-0 w-16 h-1 bg-gradient-to-r from-orange-400 to-orange-600"></span>
+        </h3>
 
-      <div className="grid grid-cols-1 xl:grid-cols-3 lg:grid-cols-2 md:gap-x-8 lg:gap-x-8 gap-y-16 md:gap-y-16 mb-4 ">
-        {communityPosts.map(({ node }) => (
-          <PostPreview
-            key={node.slug}
-            title={node.title}
-            coverImage={node.featuredImage}
-            date={node.date}
-            author={node.ppmaAuthorName}
-            slug={node.slug}
-            excerpt={getExcerpt(node.excerpt, 20)}
-            isCommunity={true}
-          />
-        ))}
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+          {communityPosts.map(({ node }) => (
+            <PostPreview
+              key={node.slug}
+              title={node.title}
+              coverImage={node.featuredImage}
+              date={node.date}
+              author={node.ppmaAuthorName}
+              slug={node.slug}
+              excerpt={getExcerpt(node.excerpt, 20)}
+              isCommunity={true}
+            />
+          ))}
+        </div>
+
+        <div className="mt-6 flex justify-end">
+          <Link
+            href="/community"
+            className="text-orange-500 hover:text-orange-600 font-semibold flex items-center transition-colors duration-300"
+          >
+            See all community blogs
+            <span className="ml-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
+              </svg>
+            </span>
+          </Link>
+        </div>
       </div>
-      <div className="w-full flex justify-end bold underline text-md mb-16">
-        <Link
-          href="/community"
-          className="text-secondary-100 hover:text-secondary-200 hover:translate-x-0.5 transition "
-        >
-          See all community blogs
-        </Link>
-      </div>
-    </>
+    </section>
   );
 };
+
 export default TopBlogs;
