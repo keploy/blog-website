3cd31482ff54244fd2dc58870377fa6d44a0f7b7
Achanandhi-M <achanandhi.m@gmail.com>
2025-04-23 14:36:25 +0530

    Added google-adsense
    Signed-off-by: Achanandhi-M <achanandhi.m@gmail.com>

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="google-adsense-account" content="ca-pub-5068394800485961" />
+      </Head>
       <body className="bg-neutral-100">
         <Main />
         <NextScript />
