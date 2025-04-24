3eb5e90cc33fad3994616ecbcf9ecff84f3f5476
Achanandhi M <110651321+Achanandhi-M@users.noreply.github.com>
2025-04-24 18:43:12 +0530

    chore: Added google analy to the blog website  (#112)
    * Added google-adsense
    
    Signed-off-by: Achanandhi-M <achanandhi.m@gmail.com>
    
    * chore/modified-google-adsense-tag
    
    Signed-off-by: Achanandhi-M <achanandhi.m@gmail.com>
    
    ---------
    
    Signed-off-by: Achanandhi-M <achanandhi.m@gmail.com>

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+      <meta name="google-adsense-account" content="ca-pub-3485005084287002" />
+      </Head>
       <body className="bg-neutral-100">
         <Main />
         <NextScript />
