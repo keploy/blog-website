ef2188ca02189f5867db94972c5e48e81877f6c0
Achanandhi-M <achanandhi.m@gmail.com>
2025-06-12 13:59:39 +0530

    fix: modified adsense component
    Signed-off-by: Achanandhi-M <achanandhi.m@gmail.com>

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,25 +1,25 @@
-// pages/_document.tsx
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
-class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="en">
-        <Head>
-          {/* Google AdSense Script */}
-          <script 
-            async 
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3485005084287002"
-            crossOrigin="anonymous"
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        {/* Google AdSense Script */}
+        <Script
+          id="adsbygoogle-init"
+          strategy="afterInteractive"
+          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3485005084287002`}
+          crossOrigin="anonymous"
+          onError={(e) => {
+            console.error('AdSense script failed to load', e);
+          }}
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
-
-export default MyDocument;
