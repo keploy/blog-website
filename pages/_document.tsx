92a9ad94a372f6d0edcd5646d8902f5c6b55f947
Achanandhi-M <achanandhi.m@gmail.com>
2025-06-12 13:53:55 +0530

    fix: modified adsense component
    Signed-off-by: Achanandhi-M <achanandhi.m@gmail.com>

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,25 @@
-import { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
+// pages/_document.tsx
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-     <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* Google AdSense Script */}
+          <script 
+            async 
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3485005084287002"
+            crossOrigin="anonymous"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
