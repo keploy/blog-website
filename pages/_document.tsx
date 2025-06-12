ca0c9024b6a6a284f3ae86b626877746f1915946
Achanandhi M <110651321+Achanandhi-M@users.noreply.github.com>
2025-06-12 08:53:23 +0530

    fix: add publisher script (#123)
    Signed-off-by: Achanandhi-M <achanandhi.m@gmail.com>

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,14 +4,7 @@ import Script from 'next/script';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <Script
-          async
-          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADS_CLIENT_ID}`}
-          strategy="lazyOnload"
-          crossOrigin="anonymous"
-        />
-      </Head>
+     <Head />
       <body>
         <Main />
         <NextScript />
