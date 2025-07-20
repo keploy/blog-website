3b52278e0aa071fffd1a01b4dfa4bbc5e135b04e
Yaten Dhingra <129659514+yaten2302@users.noreply.github.com>
2025-07-20 18:08:01 +0530

    adjust 404 text size
    Signed-off-by: Yaten Dhingra <129659514+yaten2302@users.noreply.github.com>

--- components/NotFoundPage.tsx
+++ components/NotFoundPage.tsx
@@ -9,7 +9,7 @@ const NotFoundPage = () => {
       <Container>
         <div className="flex flex-col items-center justify-center">
           <p>Page not found</p>
-          <h1 className="sm:text-[100px] text-[85px] font-normal text-[#757575]">404</h1>
+          <h1 className="text-[85px] font-normal text-[#757575]">404</h1>
           <p className="text-[18px]">
             Oops! The page you are looking for does not exist or has been moved.
           </p>
