df3dd51596bdddb532744ce7d73be1fc8b5ad80f
Yaten Dhingra <129659514+yaten2302@users.noreply.github.com>
2025-07-27 23:16:52 +0530

    init: keploy PWA setup
    Signed-off-by: Yaten Dhingra <129659514+yaten2302@users.noreply.github.com>

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,13 +6,27 @@ export default function Document() {
     <Html lang="en">
       <Head>
         {/* Google AdSense Script */}
+        <link rel="manifest" href="/blog/manifest.json" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/blog/icons/icon-192x192.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="512x512"
+          href="/blog/icons/icon-512x512.png"
+        />
+
+        <meta name="theme-color" content="#fb923c" />
         <Script
           id="adsbygoogle-init"
           strategy="afterInteractive"
           src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3485005084287002`}
           crossOrigin="anonymous"
           onError={(e) => {
-            console.error('AdSense script failed to load', e);
+            console.error("AdSense script failed to load", e);
           }}
         />
       </Head>
