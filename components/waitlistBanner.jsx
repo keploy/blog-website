1ef1af0efd63d8789894f8283c12fd4cc7c3777f
amaan-bhati <amaanbhati49@gmail.com>
2025-07-30 17:40:24 +0530

    chore: update waitlist banner link to calendar appointments
    Signed-off-by: amaan-bhati <amaanbhati49@gmail.com>

--- components/waitlistBanner.jsx
+++ components/waitlistBanner.jsx
@@ -6,7 +6,7 @@ const waitlistBanner = () => {
     <div className="flex md:items-center flex-col rounded-xl">
         <Link
           className="border border-black w-fit bg-white text-black text-base p-2 font-semibold rounded-md shadow-md sticky hover:shadow-none shadow-neutral-950  mt-4"
-          href="https://app.keploy.io/signin"
+          href="https://calendar.google.com/calendar/u/0/appointments/schedules/AcZssZ2l-psdTCNCLYAJ-Jt5ESyGP7gi1_U70ySTjtFNr0Kmx5UagNJnyzg7lNjA3NKnaP6qFfpAgcdZ"
           target="_blank"
         >
           {" "}
