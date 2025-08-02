4e85cedeef24efce58c000522c21f48b66b4d5a5
Yaten Dhingra <129659514+yaten2302@users.noreply.github.com>
2025-08-03 00:41:13 +0530

    update post preview cards
    Signed-off-by: Yaten Dhingra <129659514+yaten2302@users.noreply.github.com>

--- components/post-preview.tsx
+++ components/post-preview.tsx
@@ -38,64 +38,69 @@ export default function PostPreview({
 
   return (
     <div
-      className="group relative h-full rounded-2xl bg-[#FFFFFF] transition duration-500 flex flex-col cursor-pointer hover:border-orange-400 shadow-2xl"
+      className="group relative h-full overflow-hidden rounded-2xl border-[2px] border-[#E2E4E9] bg-[#F9FAFE] transition duration-200 hover:shadow-xl flex flex-col"
       ref={ref}
     >
-      <div className="relative pb-1 flex justify-center -translate-y-6">
+      <Link href={`${basePath}/${slug}`}>
         {coverImage && (
-          <div className="overflow-hidden rounded-lg rounded-b-2xl h-[192px] w-[calc(100%_-_32px)] shadow-lg">
-            <CoverImage
-              title={title}
-              coverImage={coverImage}
-              slug={slug}
-              isCommunity={isCommunity}
-              classNames="h-[200px] w-full transition-transform duration-500 ease-in-out"
-            />
+          <CoverImage
+            title={title}
+            coverImage={coverImage}
+            slug={slug}
+            isCommunity={isCommunity}
+            classNames="rounded-tr-2xl rounded-tl-2xl h-[200px] w-full"
+          />
+        )}
+
+        <div className="p-[24px] flex flex-col justify-between flex-1">
+          <div>
+            {tags && (
+              <span className="inline-block w-fit text-sm font-semibold bg-orange-100 text-orange-700 px-4 rounded-full py-1 mb-[8px]">
+                {tags}
+              </span>
+            )}
+
+            <h3 className="leading-tight font-medium text-lg 2xl:text-xl block group-hover:underline">
+              <span
+                className="bg-[length:0px_10px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500 hover:bg-[length:100%_10px] group-hover:bg-[length:100%_10px] hover:underline"
+                dangerouslySetInnerHTML={{ __html: title }}
+              ></span>
+            </h3>
           </div>
-        )}
-      </div>
+        </div>
 
-      <div className="px-[24px] pb-[24px] flex flex-col justify-between flex-1">
-        {tags && (
-          <span className="inline-block w-fit text-sm font-semibold bg-orange-100 text-orange-700 px-4 rounded-full py-1 mb-[8px]">
-            {tags}
-          </span>
-        )}
-
-        <h3 className="leading-tight font-medium text-lg 2xl:text-xl block group-hover:underline">
-          <Link
-            href={`${basePath}/${slug}`}
-            className="bg-[length:0px_10px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500 hover:bg-[length:100%_10px] group-hover:bg-[length:100%_10px]"
-            dangerouslySetInnerHTML={{ __html: title }}
-          ></Link>
-        </h3>
-      </div>
-
-      <div className="px-[1.5rem] pt-0 pb-[1rem] xl:pb-[1.5rem]">
-        <div className="flex items-center gap-4">
-          <div className="flex flex-row justify-center items-center">
-            <Image
-              src={
-                authorImage &&
-                authorImage !== "imag1" &&
-                authorImage !== "image"
-                  ? authorImage
-                  : `/blog/images/author.png`
-              }
-              alt={`${author}'s Avatar`}
-              className="w-10 h-10 rounded-full mr-3 sm:mr-2"
-              height={40}
-              width={40}
-            />
-            <div>
-              <Avatar author={author ? author : "Anonymous"} />
-              <div className="text-[12px] mb-0 -mt-1 text-gray-500">
-                <Date dateString={date} />
+        <div className="p-[1rem] pt-0 xl:p-[1.5rem] xl:pt-0">
+          <div className="flex items-center gap-4">
+            <div className="flex flex-row justify-center items-center">
+              {authorImage &&
+              authorImage !== "imag1" &&
+              authorImage !== "image" ? (
+                <Image
+                  src={authorImage}
+                  alt={`${authorImage}'s Avatar`}
+                  className="w-10 h-10 rounded-full mr-3 sm:mr-2"
+                  height={40}
+                  width={40}
+                />
+              ) : (
+                <Image
+                  src={`/blog/images/author.png`}
+                  alt={`${authorImage}'s Avatar`}
+                  className="w-10 h-10 rounded-full mr-3 sm:mr-2"
+                  height={40}
+                  width={40}
+                />
+              )}
+              <div>
+                <Avatar author={author ? author : "Anonymous"} />
+                <div className="text-[12px] mb-0 -mt-1 text-gray-500">
+                  <Date dateString={date} />
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 }
