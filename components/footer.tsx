03c377723bfaa91f7a11d9c17faa6b6646bf506a
Gourav <gouravbarwal001@gmail.com>
2025-09-22 20:19:36 +0530

    Add Link

--- components/footer.tsx
+++ components/footer.tsx
@@ -78,6 +78,54 @@ export default function Footer() {
           <ul className="flex mb-4 md:order-1 md:ml-4 md:mb-0">
             <li>
               <Link
+                href = "https://www.linkedin.com/company/keploy/"
+                className = "flex justify-center items-center text-secondary-300 hover:text-primary-300 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out"
+                aria-label = "Linkedin"
+                target = "_blank"
+              >
+                 <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="28"
+                  height="28"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M19 0h-14c-2.761...z" /> {/* LinkedIn icon path */}
+                </svg>
+              </Link>
+              <Link
+                href = "https://www.instagram.com/keploy.io/"
+                className = "flex justify-center items-center text-secondary-300 hover:text-primary-300 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out"
+                aria-label = "Instagram"
+                target = "_blank"
+              >
+                <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="28"
+                height="28"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path d="M7.75 2h8.5A5.75...z" /> {/* Instagram icon path */}
+              </svg>
+              </Link>
+              <Link
+                href = "https://www.youtube.com/@keploy"
+                className = "flex justify-center items-center text-secondary-300 hover:text-primary-300 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out"
+                aria-label = "Youtube"
+                target = "_blank"
+              >
+                 <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="28"
+                  height="28"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M19.615 3.184c...z" /> {/* YouTube icon path */}
+                </svg>
+              </Link>
+              <Link
                 href="https://twitter.com/Keployio"
                 className="flex justify-center items-center text-secondary-300 hover:text-primary-300 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out"
                 aria-label="Twitter"
@@ -113,6 +161,7 @@ export default function Footer() {
                   <path d="M16 8.2c-4.4 0-8 3.6-8 8 0 3.5 2.3 6.5 5.5 7.6.4.1.5-.2.5-.4V22c-2.2.5-2.7-1-2.7-1-.4-.9-.9-1.2-.9-1.2-.7-.5.1-.5.1-.5.8.1 1.2.8 1.2.8.7 1.3 1.9.9 2.3.7.1-.5.3-.9.5-1.1-1.8-.2-3.6-.9-3.6-4 0-.9.3-1.6.8-2.1-.1-.2-.4-1 .1-2.1 0 0 .7-.2 2.2.8.6-.2 1.3-.3 2-.3s1.4.1 2 .3c1.5-1 2.2-.8 2.2-.8.4 1.1.2 1.9.1 2.1.5.6.8 1.3.8 2.1 0 3.1-1.9 3.7-3.7 3.9.3.4.6.9.6 1.6v2.2c0 .2.1.5.6.4 3.2-1.1 5.5-4.1 5.5-7.6-.1-4.4-3.7-8-8.1-8z"/>
                 </svg>
               </Link>
+              
             </li>
           </ul>
           <div className="text-sm text-neutral-300 mr-4">
