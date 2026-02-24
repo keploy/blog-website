ecb1a56359305d00c3f700fcb38bb8b899bfb039
Heet Mehta <heetmehta18125@gmail.com>
2026-02-24 22:00:47 +0530

    refactor: remove unused useCallback and useMemo imports
    Signed-off-by: Heet Mehta <heetmehta18125@gmail.com>
    Signed-off-by: heetmehta18 <heetmehta18125@gmail.com>

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useMemo } from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/router";
 import { Marquee } from "./Marquee";
 import Tweets from "../services/Tweets";
