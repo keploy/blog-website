d18281a60d4f49430211335f99cf2bb129a36fb2
dhananjay6561 <dhananjayaggarwal6561@gmail.com>
2026-02-28 10:55:36 +0530

    fix: UI breaking at some dimensions, made it responsive
    Signed-off-by: dhananjay6561 <dhananjayaggarwal6561@gmail.com>

--- components/post-body.tsx
+++ components/post-body.tsx
@@ -326,13 +326,12 @@ export default function PostBody({
         } `}
     >
       <div
-        className={`flex items-center justify-center w-full md:w-[80%] lg:w-1/4 top-20 lg:block ${isList ? "" : "lg:sticky"
+        className={`flex items-center justify-center w-full md:w-[80%] lg:w-[340px] lg:shrink-0 top-20 lg:block ${isList ? "" : "lg:sticky"
           }`}
       >
         <TOC headings={tocItems} isList={isList} setIsList={setIsList} />
       </div>
-      {/* <div className={`w-full p-4 pr-4 lg:pr-4 ${isList ? "ml-10" : "lg:ml-8"}  md:w-4/5 lg:w-3/5`} id="post-body-check"> */}
-      <div className={`p-4 ${isList ? "ml-10" : ""} mx-auto`} style={{ maxWidth: '800px' }} id="post-body-check">
+      <div className={`p-4 ${isList ? "lg:ml-10" : ""} mx-auto flex-1 min-w-0 w-full`} style={{ maxWidth: '800px' }} id="post-body-check">
         {slug === "how-to-compare-two-json-files" && <JsonDiffViewer />}
         <div className="prose lg:prose-xl post-content-wrapper">{renderCodeBlocks()}</div>
         <hr className="border-gray-300 mt-10 mb-20" />
