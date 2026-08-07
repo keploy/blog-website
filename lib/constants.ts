f7aed4c900ef03f18f6121212abdf54aa4916da3
dhananjay6561 <dhananjayaggarwal6561@gmail.com>
2026-08-07 15:15:58 +0530

    chore: remove 6 unused images and their dead thumbnail import
    Signed-off-by: dhananjay6561 <dhananjayaggarwal6561@gmail.com>

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,3 @@
-import BlogThumbnil from '../public/images/thumbnil.png';
 export const EXAMPLE_PATH = 'cms-wordpress'
 export const CMS_NAME = 'WordPress'
 export const CMS_URL = 'https://wordpress.org'
